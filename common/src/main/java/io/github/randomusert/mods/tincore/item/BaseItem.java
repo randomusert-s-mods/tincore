1c6bbeb4ccfdc81d568fe0702648f0b58c53f239
randomusert <127417963+randomusert@users.noreply.github.com>
2026-04-02 19:06:21 +0300

    BaseItem class

---
+++ common/src/main/java/io/github/randomusert/mods/tincore/item/BaseItem.java
@@ -0,0 +1,9 @@
+package io.github.randomusert.mods.tincore.item;
+
+import net.minecraft.world.item.Item;
+
+public class BaseItem extends Item {
+    public BaseItem(Properties properties) {
+        super(properties);
+    }
+}
